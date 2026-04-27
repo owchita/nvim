febdc0dcfdbc19e7ee131eb7f94c2bc601f94824
owchita <114903732+owchita@users.noreply.github.com>
2026-04-28 01:20:23 +0300

    Initial commit

---
+++ lua/config/options.lua
@@ -0,0 +1,30 @@
+-- Appearance
+vim.opt.cursorline = true
+vim.opt.number = true
+
+-- Mouse
+vim.opt.mouse = ""
+
+-- Search
+vim.opt.ignorecase = true
+vim.opt.incsearch = true
+vim.opt.smartcase = true
+
+-- Filetypes and indentation
+vim.opt.breakindent = true
+vim.opt.filetype = "on"
+vim.opt.linebreak = true
+vim.opt.relativenumber = true
+vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
+
+-- Splitting
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+-- Misc
+vim.opt.scrolloff = 5
+vim.opt.title = true
+
+-- Clipboard
+vim.opt.clipboard = "unnamedplus"
