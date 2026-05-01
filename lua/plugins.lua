134dd16f4c8f0b8bbbcdf63633bd1371eeb32c7c
owchita <114903732+owchita@users.noreply.github.com>
2026-05-01 21:32:57 +0300

    Add surround plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,19 +1,26 @@
 return {
     { -- Autopairs
         "windwp/nvim-autopairs",
+
         event = "InsertEnter",
         config = true
     },
 
     { -- Colorscheme
         "vague-theme/vague.nvim",
+
         config = function()
             vim.cmd.colorscheme "vague"
         end
     },
 
+    { -- Comment
+        "numtostr/comment.nvim",
+    },
+
     { -- Highlight colors
         "brenoprata10/nvim-highlight-colors",
+
         config = function()
             require("nvim-highlight-colors").setup({})
         end
@@ -21,20 +28,23 @@ return {
 
     { -- Lualine
         "nvim-lualine/lualine.nvim",
+
         dependencies = {
             "nvim-tree/nvim-web-devicons",
         },
+
         opts = {
             theme = "vague",
         },
     },
 
-    { -- Comment
-        "numtostr/comment.nvim",
+    { --Surround
+        "kylechui/nvim-surround",
     },
 
     { -- Telescope
         'nvim-telescope/telescope.nvim', version = '*',
+
         dependencies = {
             'nvim-lua/plenary.nvim',
             { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
@@ -42,6 +52,7 @@ return {
 
         config = function()
             local builtin = require('telescope.builtin')
+
             vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
             vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
             vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
@@ -50,6 +61,7 @@ return {
 
     { -- Zen Mode
         "folke/zen-mode.nvim",
+
         vim.keymap.set("n", "<leader>z", vim.cmd.ZenMode)
     },
 }
