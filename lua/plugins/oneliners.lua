cb9cf9fc880557d9806027930f02c12a072732b0
owchita <114903732+owchita@users.noreply.github.com>
2026-09-12 13:12:22 +0300

    remove redundant plugin

--- lua/plugins/oneliners.lua
+++ lua/plugins/oneliners.lua
@@ -7,10 +7,6 @@ return {
     --     "lewis6991/gitsigns.nvim",
     -- },
 
-    { -- nvim-lspconfig
-        "neovim/nvim-lspconfig",
-    },
-
     { --Surround
         "kylechui/nvim-surround",
     },
